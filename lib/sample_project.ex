ecd9f7395413d56fc5917e6bd7d59285c1ef1eb6
Proto <humayoon04syed@gmail.com>
2025-01-30 22:49:28 +0500

    BubbleSort algo Basic

--- lib/sample_project.ex
+++ lib/sample_project.ex
@@ -1,18 +1,15 @@
-defmodule SampleProject do
-  @moduledoc """
-  Documentation for `SampleProject`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> SampleProject.hello()
-      :world
-
-  """
-  def hello do
-    :world
+defmodule BubbleSort do
+  def sort([]), do: []
+  def sort([head | tail]) do
+    sorted_tail = sort(tail)
+    insert(head, sorted_tail)
   end
-end
+
+  defp insert(x, []), do: [x]
+  defp insert(x, [head | tail]) when x <= head do
+    [x | [head | tail]]
+  end
+  defp insert(x,[head | tail]) do
+    [head | insert(x,tail)]
+  end
+  end
--- test/sample_project_test.exs
+++ test/sample_project_test.exs
@@ -1,8 +1,15 @@
-defmodule SampleProjectTest do
-  use ExUnit.Case
-  doctest SampleProject
-
-  test "greets the world" do
-    assert SampleProject.hello() == :world
+defmodule BubbleSort do
+  def sort([]), do: []
+  def sort([head | tail]) do
+    sorted_tail = sort(tail)
+    insert(head, sorted_tail)
   end
-end
+
+  defp insert(x, []), do: [x]
+  defp insert(x, [head | tail]) when x <= head do
+    [x | [head | tail]]
+  end
+  defp insert(x,[head | tail]) do
+    [head | insert(x,tail)]
+  end
+  end
